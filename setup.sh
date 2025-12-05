6ca263c627270c2a1195ff391058c1a4a5f61e01
Atomicbeast101 <adam@brodack.net>
2025-12-04 19:57:03 -0600

    Test installing ansible-pylibssh w/o building binary

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 PYTHON_SITE_PACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`
 .venv/bin/python3 -m pip install -r requirements.txt
+.venv/bin/python3 -m pip install --no-binary ansible-pylibssh # Install additional package for connecting to HP switches using ansible collection - skips needing CC language which seems to not work on alpine regardless of installing dependencies for it.
 .venv/bin/ansible-galaxy collection install -p $PYTHON_SITE_PACKAGES/ansible_collections -r requirements.yml --force
 deactivate
