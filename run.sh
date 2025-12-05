b8b97183fdbbabb693d94231001d5bd0f69018ff
Atomicbeast101 <adam@brodack.net>
2025-12-05 14:19:53 -0600

    Test w/ NETWORK_GROUP_MODULES

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@
 echo "Performing backup..."
 PYTHON_SITE_PACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`
 export ANSIBLE_COLLECTIONS_PATH=$PYTHON_SITE_PACKAGES/ansible_collections
+export NETWORK_GROUP_MODULES=arubaoss
 .venv/bin/ansible-playbook playbook.yml -i $SWITCH_HOST,
 echo "Backup completed!"
