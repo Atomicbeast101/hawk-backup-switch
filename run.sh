5f83af8834999272e9a790f121c4d794c4199acc
Atomicbeast101 <adam@brodack.net>
2025-12-22 09:48:00 -0600

    Switched to supercronic from cron

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Run ansible playbook
 echo "Performing backup..."
+
+# Run ansible playbook
 PYTHON_SITE_PACKAGES=`python -c 'import site; print(site.getsitepackages()[0])'`
 export ANSIBLE_COLLECTIONS_PATH=$PYTHON_SITE_PACKAGES/ansible_collections
 export NETWORK_GROUP_MODULES=arubaoss
 .venv/bin/ansible-playbook playbook.yml -i $SWITCH_HOST,
+
 echo "Backup completed!"
